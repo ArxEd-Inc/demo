7caf4a5ef2cf3b89181294734954dfe4a7b57962
Santiago Casanova <santiago.casanova@yahoo.com>
2024-06-27 14:24:17 -0600

    new plots for trends

--- scripts/qr_code_figs.R
+++ scripts/qr_code_figs.R
@@ -118,7 +118,7 @@ plotly_plot <- ggplotly(p) %>%
       y=26500,
       xref = "x",
       yref = "y",
-      text = "Click the dots to learn more!",
+      text = "Click on the dots to learn more!",
       xanchor = 'right',
       showarrow = F
     )
@@ -277,7 +277,7 @@ plotly_plot <- ggplotly(p) %>%
     y=26500,
     xref = "x",
     yref = "y",
-    text = "Click the dots to learn more!",
+    text = "Click on the dots to learn more!",
     xanchor = 'right',
     showarrow = F
   )
@@ -302,4 +302,262 @@ plotly_plot$x$data[[4]]$hovertext <- paste(
 htmlwidgets::saveWidget(config(plotly_plot, showLink = F, displayModeBar = FALSE),
                         "~/Documents/Github/demo/assets/plots/trend.html")
 
+
+# Low and high -----------------------------------------------------------
+
+lowe_high_data <- read_csv('salary_info_full_june.csv') %>% 
+  filter(year %in% c('2020-21', '2021-22', '2022-23', '2023-24', '2024-25')) %>% 
+  group_by(year) %>% 
+  summarise(
+    starting_b = min(lowest_step_bachelors, na.rm = T),
+    starting_m = min(lowest_step_masters, na.rm = T),
+    starting_h = min(lowest_step_highest_lane, na.rm = T),
+    top_b = max(highest_step_bachelors, na.rm = T),
+    top_m = max(highest_step_masters, na.rm = T),
+    top_h = max(highest_step_highest_lane, na.rm = T),
+  ) %>% 
+  mutate(
+    lab1 = c(
+      '2020-21',
+      '',
+      '',
+      '',
+      '2024-25'
+    ),
+    increase_bmin = starting_b/lag(starting_b) -1,
+    increase_mmin = starting_m/lag(starting_m) -1,
+    increase_hmin = starting_h/lag(starting_h) -1,
+    increase_bmax = top_b/lag(top_b) -1,
+    increase_mmax = top_m/lag(top_m) -1,
+    increase_hmax = top_h/lag(top_h) -1
+  )
+
+
+
+p <- ggplot(lowe_high_data, aes(x = year, group = 1)) +
+  geom_line(aes(y = starting_b),
+            color = '#4E0F53'
+            ) +
+  geom_line(aes(y = starting_m),
+            color = '#4E0F53' ,
+            )+
+  geom_line(aes(y = starting_h),
+            color = '#DC9542' 
+            )+
+  geom_point(aes(y = starting_b),
+             size = 3,
+             color = '#4E0F53' ,
+  ) +
+  geom_point(aes(y = starting_m),
+             size = 3,
+             color = '#27396A' ,
+  ) +
+  geom_point(aes(y = starting_h),
+             size = 3,
+             color = '#DC9542' 
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none",
+        text = element_text(family = "Arial Rounded MT Bold"),
+        axis.text.y = element_text(size = 13),
+        axis.text.x = element_blank(),
+        # panel.grid.minor.x = element_blank()
+        panel.grid.major.x = element_blank()
+  ) +
+  coord_cartesian(clip="off") +
+  scale_y_continuous(labels = numberFormattR::suffix_formatter_0)+
+  geom_text(
+    aes(y = starting_h+1000, 
+        family = "Arial Rounded MT Bold",
+        label = lab1)
+  )+
+  # geom_text(
+  #   aes(y = low-4000, 
+  #       family = "Arial Rounded MT Bold",
+  #       label = lab2)
+  # )+
+  expand_limits(x= c(0.1, length(levels(as.factor(lowe_high_data$year))) + 1.5))
+
+
+
+plotly_plot <- ggplotly(p) %>% 
+  plotly::layout(
+    xaxis = list(title = "", 
+                 fixedrange = T, 
+                 automargin = T
+    ),
+    margin = list(l = 10, r = 10, b = 40, t = 10),
+    yaxis = list(title = "", fixedrange = T, automargin = T)
+  ) %>% 
+  add_annotations(
+    x=5.4,
+    y=49814,
+    xref = "x",
+    yref = "y",
+    text = "Highest Lane\nStep 1",
+    xanchor = 'left',
+    showarrow = F,
+    font = list(color = 'black', size = 13)
+  ) %>%
+  add_annotations(
+    x=5.4,
+    y=46068,
+    xref = "x",
+    yref = "y",
+    text = "Masters\nStep 1",
+    xanchor = 'left',
+    showarrow = F,
+    font = list(color = 'black', size = 13)
+  ) %>% 
+  add_annotations(
+    x=5.4,
+    y=42752,
+    xref = "x",
+    yref = "y",
+    text = "Bachelors\nStep 1",
+    xanchor = 'left',
+    showarrow = F,
+    font = list(color = 'black', size = 13)
+  ) 
+
+plotly_plot$x$data[[1]]$text <- paste(
+  # "Step:",salary_data$type, "<br>",
+  "Starting Bachelors:", scales::dollar(lowe_high_data$starting_b), "<br>",
+  'Increase:', scales::percent(round(lowe_high_data$increase_bmin, 3))
+)
+
+plotly_plot$x$data[[2]]$text <- paste(
+  # "Step:",salary_data$type, "<br>",
+  "Starting Masters:", scales::dollar(lowe_high_data$starting_m), "<br>",
+  'Increase:', scales::percent(round(lowe_high_data$increase_mmin, 3))
+)
+
+plotly_plot$x$data[[3]]$text <- paste(
+  # "Step:",salary_data$type, "<br>",
+  "Starting Highest Lane:", scales::dollar(lowe_high_data$starting_h), "<br>",
+  'Increase:', scales::percent(round(lowe_high_data$increase_hmin, 3))
+)
+
+plotly_plot$x$data[[4]]$hovertext <- paste(
+  # "Step:",salary_data$type, "<br>",
+  lowe_high_data$year)
+
+htmlwidgets::saveWidget(config(plotly_plot, showLink = F, displayModeBar = FALSE),
+                        "~/Documents/Github/demo/assets/plots/starting.html")
+
+
+# highest -----------------------------------------------------------------
+
+
+p <- ggplot(lowe_high_data, aes(x = year, group = 1)) +
+  geom_line(aes(y = top_b),
+            color = '#4E0F53'
+  ) +
+  geom_line(aes(y = top_m),
+            color = '#4E0F53' ,
+  )+
+  geom_line(aes(y = top_h),
+            color = '#DC9542' 
+  )+
+  geom_point(aes(y = top_b),
+             size = 3,
+             color = '#4E0F53' ,
+  ) +
+  geom_point(aes(y = top_m),
+             size = 3,
+             color = '#27396A' ,
+  ) +
+  geom_point(aes(y = top_h),
+             size = 3,
+             color = '#DC9542' 
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none",
+        text = element_text(family = "Arial Rounded MT Bold"),
+        axis.text.y = element_text(size = 13),
+        axis.text.x = element_blank(),
+        # panel.grid.minor.x = element_blank()
+        panel.grid.major.x = element_blank()
+  ) +
+  coord_cartesian(clip="off") +
+  scale_y_continuous(labels = numberFormattR::suffix_formatter_0)+
+  geom_text(
+    aes(y = top_h+3000, 
+        family = "Arial Rounded MT Bold",
+        label = lab1)
+  )+
+  # geom_text(
+  #   aes(y = low-4000, 
+  #       family = "Arial Rounded MT Bold",
+  #       label = lab2)
+  # )+
+  expand_limits(x= c(0.1, length(levels(as.factor(lowe_high_data$year))) + 1.5))
+
+
+
+plotly_plot <- ggplotly(p) %>% 
+  plotly::layout(
+    xaxis = list(title = "", 
+                 fixedrange = T, 
+                 automargin = T
+    ),
+    margin = list(l = 10, r = 10, b = 40, t = 10),
+    yaxis = list(title = "", fixedrange = T, automargin = T)
+  ) %>% 
+  add_annotations(
+    x=5.4,
+    y=134432,
+    xref = "x",
+    yref = "y",
+    text = "Highest Lane\nHighest Step",
+    xanchor = 'left',
+    showarrow = F,
+    font = list(color = 'black', size = 13)
+  ) %>%
+  add_annotations(
+    x=5.4,
+    y=118596,
+    xref = "x",
+    yref = "y",
+    text = "Masters\nHighest Step1",
+    xanchor = 'left',
+    showarrow = F,
+    font = list(color = 'black', size = 13)
+  ) %>% 
+  add_annotations(
+    x=5.4,
+    y=111090,
+    xref = "x",
+    yref = "y",
+    text = "Bachelors\nHighest Step",
+    xanchor = 'left',
+    showarrow = F,
+    font = list(color = 'black', size = 13)
+  ) 
+
+
+plotly_plot$x$data[[1]]$text <- paste(
+  # "Step:",salary_data$type, "<br>",
+  "Max Pay Bachelors:", scales::dollar(lowe_high_data$starting_b), "<br>",
+  'Increase:', scales::percent(round(lowe_high_data$increase_bmin, 3))
+)
+
+plotly_plot$x$data[[2]]$text <- paste(
+  # "Step:",salary_data$type, "<br>",
+  "Max Pay Masters:", scales::dollar(lowe_high_data$starting_m), "<br>",
+  'Increase:', scales::percent(round(lowe_high_data$increase_mmin, 3))
+)
+
+plotly_plot$x$data[[3]]$text <- paste(
+  # "Step:",salary_data$type, "<br>",
+  "Max Pay Highest Lane:", scales::dollar(lowe_high_data$starting_h), "<br>",
+  'Increase:', scales::percent(round(lowe_high_data$increase_hmin, 3))
+)
+
+plotly_plot$x$data[[4]]$hovertext <- paste(
+  # "Step:",salary_data$type, "<br>",
+  lowe_high_data$year)
+
+htmlwidgets::saveWidget(config(plotly_plot, showLink = F, displayModeBar = FALSE),
+                        "~/Documents/Github/demo/assets/plots/max.html")
 
